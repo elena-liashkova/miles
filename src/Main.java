d7ab79dfff7f532a40daca732eb92a58c949a7c3
Elena Liashkova <elena.lyashkova@gmail.com>
2020-09-19 18:00:21 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        float ticketPrice = 805F;
+        int mileCost = 20;
+        int mileBonus =(int)ticketPrice / mileCost;
+        System.out.println(mileBonus);
+    }
+}
+
