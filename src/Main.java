c64173637a8604e95a392cdfec692184a5cdab3c
Elena Liashkova <elena.lyashkova@gmail.com>
2020-09-19 18:10:29 +0300

    forth change price

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        float ticketPrice = 10F;
+        float ticketPrice = 53F;
         int mileCost = 20;
         int mileBonus =(int)ticketPrice / mileCost;
         System.out.println(mileBonus);
