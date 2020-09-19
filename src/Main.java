9d6efd2e218fb9f32349f10353a80281c0f5a937
Elena Liashkova <elena.lyashkova@gmail.com>
2020-09-19 18:02:22 +0300

    first change price

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        float ticketPrice = 805F;
+        float ticketPrice = 53F;
         int mileCost = 20;
         int mileBonus =(int)ticketPrice / mileCost;
         System.out.println(mileBonus);
